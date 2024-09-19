6f492143d47b2e6737a368d13dc74502df8a381b
lautyjs <lautyhoff77@gmail.com>
2024-09-18 21:16:04 -0300

    Entrega incompleta 18/09

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -5,9 +5,16 @@ socket.on("products", (data) => {
     .map(
       (each) =>
         ` 
-      <img style="width: 33%; height: 33%" src="${each.photo}" 
-      
-      class="object-fit-cover" alt="${each.id}"> `
+      <div style="width: 30vw; heigth:200px; backgroung-color: gray 0,5">
+      <img style="width:100%;padding: 2px; heigth: 90%" src="${each.photo}" class="object-fit-cover" alt="${each.id}"> 
+
+      <h3 style= "" src="${each.title}">
+      ${each.title}
+      </h3>
+      </div>
+
+
+      `
     )
     .splice(0, 9)
     .join("");
